f927bc1ab4776aa3ce76c768be82aca5d0240450
Fotis <fotanftm@gmail.com>
2014-12-02 21:37:31 +0200

    warming up

---
+++ virMac.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+#include <pthread.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+int * code;
+int curr;
+
+
+int main (int argc, char * argv[]){
+  
+  if (argc < 2){ 
+    printf ("Not enough arguments\n");
+  }
+  
+  //----------read header file---------------
+  
+  
+  
+  //---------initialize globals------------
+  string code[]
+  list of tasks[]
+  int currExecTask
+  
+  
+  //---------store bodies into string------------
+  
+  //----------read task parameters etc----------
+  
+  
+  
+  //----------execute tasks---------------------
+  
+  
+  return (0);
+}
