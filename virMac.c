feb898948ac084a444ffcc87ecdbab8099387f16
siderisng <freelook.gr@gmail.com>
2014-12-03 14:48:23 +0200

    case completed

--- virMac.c
+++ virMac.c
@@ -12,12 +12,20 @@ int * globalMem;
 typedef struct task{
 	int body;
 	int arg;
+	int id;
+	char state[10];
+	int reg[8];
+	int pc;
+	int sem;
+	int waket;
+	int * localMem;
 }taskT;
 
 int main (int argc, char * argv[]){
 	
 	FILE * fp;
 	
+	int command[3];
 	int i,k,flag=0;
 	int magicbeg[4];
 	int globalsize;
@@ -204,12 +212,31 @@ int main (int argc, char * argv[]){
 		fscanf(fp,"%x",&tasks[k].body);
 		printf("%x", tasks[k].body);
 		printf("\n");
+		
 
 		//------argument------------
 		fscanf(fp,"%x",&tasks[k].arg);
 		printf("%x", tasks[k].arg);
 		printf("\n");
+		
+		//------initialize-----------
+		tasks[k].id=k;
+		strcpy(tasks[k].state,"READY");
+		tasks[k].pc=0;
+		tasks[k].sem=-1;
+		tasks[k].waket=-1;
+		
+		if (NULL==(tasks[k].localMem=((int*)malloc (sizeof(int)*localSize[(tasks[k].body)])))){
+			perror("malloc error");
+			return (1);
+		}
+		
+		
+		tasks[k].localMem[(localSize[(tasks[k].body)])]=tasks[k].arg;
+		
+		
 	}
+	
 	
 	//-----------footer---------------------
 	
@@ -244,7 +271,143 @@ int main (int argc, char * argv[]){
 	
 	
 	//----------execute tasks---------------------
-	
+	curr=0;
+	int progress=0;
+	k=0;
+	while (k<7){
+		for (i=0;i<3;i++){
+			command[i]=code[(tasks[curr].body)-1][tasks[curr].pc];
+			printf("%x ",command[i]);
+			tasks[curr].pc++;
+		}
+		printf("\n");
+		
+		switch(command[0]){
+			// -----------load/store----------------------------
+			case 0x01 :
+				tasks[curr].reg[command[1]]=tasks[curr].localMem[command[2]];
+				printf("%x\n",tasks[curr].reg[command[1]]);
+				progress++;
+				break;
+			case 0x02 :
+				tasks[curr].reg[command[1]]=tasks[curr].localMem[command[2]+tasks[curr].reg[0]];
+				printf("%x\n",tasks[curr].reg[command[1]]);
+				progress++;
+				break;
+			case 0x03 :
+				tasks[curr].reg[command[1]]=globalMem[command[2]];
+				printf("%x\n",tasks[curr].reg[command[1]]);
+				progress++;
+				break;
+			case 0x04 :
+				tasks[curr].reg[command[1]]=globalMem[command[2]+tasks[curr].reg[0]];
+				printf("%x\n",tasks[curr].reg[command[1]]);
+				progress++;
+				break;
+			case 0x05 :
+				tasks[curr].localMem[command[2]]=tasks[curr].reg[command[1]];
+				printf("%x\n",tasks[curr].reg[command[1]]);
+				progress++;
+				break;
+			case 0x06 :
+				tasks[curr].localMem[command[2]+tasks[curr].reg[0]]=tasks[curr].reg[command[1]];
+				printf("%x\n",tasks[curr].reg[command[1]]);
+				progress++;
+				break;
+			case 0x07 :
+				globalMem[command[2]]=tasks[curr].reg[command[1]];
+				printf("%x\n",tasks[curr].reg[command[1]]);
+				progress++;
+				break;
+			case 0x08 :
+				globalMem[command[2]+tasks[curr].reg[0]]=tasks[curr].reg[command[1]];
+				printf("%x\n",tasks[curr].reg[command[1]]);
+				progress++;
+				break;
+			//-----------registers--------------------
+			case 0x09:
+				tasks[curr].reg[command[1]]=command[2];
+				progress++;
+				break;
+			case 0x0A:
+				tasks[curr].reg[command[1]]=tasks[curr].reg[command[1]]+tasks[curr].reg[command[2]];
+				progress++;
+				break;
+			case 0x0B:
+				tasks[curr].reg[command[1]]=tasks[curr].reg[command[1]]-tasks[curr].reg[command[2]];
+				progress++;
+				break;
+			case 0x0C:
+				tasks[curr].reg[command[1]]=tasks[curr].reg[command[1]]*tasks[curr].reg[command[2]];
+				progress++;
+				break;
+			case 0x0D:
+				tasks[curr].reg[command[1]]=tasks[curr].reg[command[1]]/tasks[curr].reg[command[2]];
+				progress++;
+				break;
+			case 0x0E:
+				tasks[curr].reg[command[1]]=tasks[curr].reg[command[1]]%tasks[curr].reg[command[2]];
+				progress++;
+				break;
+				
+			// --------- branches---------------------
+				
+			case 0x0F:
+				if (tasks[curr].reg[command[1]]>0){
+					tasks[curr].pc=tasks[curr].pc+command[2];
+				}
+				progress++;
+				break;
+			case 0x10:
+				if (tasks[curr].reg[command[1]]>=0){
+					tasks[curr].pc=tasks[curr].pc+command[2];
+				}
+				progress++;
+				break;
+			case 0x11:
+				if (tasks[curr].reg[command[1]]<0){
+					tasks[curr].pc=tasks[curr].pc+command[2];
+				}
+				progress++;
+				break;
+			case 0x12:
+				if (tasks[curr].reg[command[1]]<=0){
+					tasks[curr].pc=tasks[curr].pc+command[2];
+				}
+				progress++;
+				break;
+			case 0x13:
+				if (tasks[curr].reg[command[1]]==0){
+					tasks[curr].pc=tasks[curr].pc+command[2];
+				}
+				progress++;
+				break;
+			case 0x14:
+				tasks[curr].pc=tasks[curr].pc+command[2];
+				progress++;
+				break;
+				
+			// ----------- synch ------------------
+			case 0x15:
+				globalMem[command[2]]--;
+				if (globalMem[command[2]]<0) {
+					strcpy(tasks[curr].state,"BLOCKED");
+				}
+				tasks[curr].sem=command[2];
+				progress++;
+				break;
+			/*case 0x16:
+				globalMem[command[2]]++;
+				if (globalMem[command[2]]<=0) {
+				
+					strcpy(tasks[curr].state,"BLOCKED");
+				}
+				tasks[curr].sem=command[2];
+				progress++;
+				break;*/
+		}
+		k++;	
+	}
 	
 	
 	
