f459f900eb03a20eb36bc4a6ebe0c52cdba5d950
Fotis <fotanftm@gmail.com>
2014-12-05 18:09:06 +0200

    basic structure complete... all is left is troubleshooting and improvments

--- virMac.c
+++ virMac.c
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdint.h>
+#include <time.h>
 
 uint8_t ** code;
 int curr;
@@ -14,19 +15,19 @@ typedef struct task{
 	uint8_t arg;
 	int id;
 	char state[10];
-	int reg[8];
-	int pc;
-	int sem;
-	int waket;
-	int * localMem;
+	uint8_t reg[8];
+	uint8_t pc;
+	uint8_t sem;
+	time_t waket;
+	uint8_t * localMem;
 }taskT;
 
 int main (int argc, char * argv[]){
 	
+	srand(time(NULL));
 	FILE * fp;
-	
 	uint8_t command[3];
-	int i,k,flag=0;
+	int i,k,flag=0, endflag=0;
 	uint8_t magicbeg[4];
 	uint8_t globalsize;
 	uint8_t numofbodies;
@@ -51,7 +52,7 @@ int main (int argc, char * argv[]){
 		
 		printf("%x", magicbeg[i]);
 		
-		}
+	}
 	printf("\n");
 	
 	
@@ -236,7 +237,7 @@ int main (int argc, char * argv[]){
 		tasks[k].sem=-1;
 		tasks[k].waket=-1;
 		
-		if (NULL==(tasks[k].localMem=((int*)malloc (sizeof(int)*localSize[(tasks[k].body)])))){
+		if (NULL==(tasks[k].localMem=((uint8_t*)malloc (sizeof(uint8_t)*localSize[(tasks[k].body)])))){
 			perror("malloc error");
 			return (1);
 		}
@@ -284,57 +285,58 @@ int main (int argc, char * argv[]){
 	curr=0;
 	int progress=0;
 	k=0;
-	while (k<8){
+	while (1){
 		for (i=0;i<3;i++){
 			command[i]=code[(tasks[curr].body)-1][tasks[curr].pc];
-			printf("%x ",command[i]);
+			//printf("%x ",command[i]);
 			tasks[curr].pc++;
 		}
-		printf("\n");
-		
+		//printf("\n");
+		printf ("HIII from task :%d state:%s notasks:%d\n", tasks[curr].id,tasks[curr].state, notasks);	
+
 		switch(command[0]){
 			// -----------load/store----------------------------
 			case 0x01 :
 				tasks[curr].reg[command[1]]=tasks[curr].localMem[command[2]];
-				printf("%x\n",tasks[curr].reg[command[1]]);
+				//	printf("%x\n",tasks[curr].reg[command[1]]);
 				progress++;
 				break;
 			case 0x02 :
 				tasks[curr].reg[command[1]]=tasks[curr].localMem[command[2]+tasks[curr].reg[0]];
-				printf("%x\n",tasks[curr].reg[command[1]]);
+				//	printf("%x\n",tasks[curr].reg[command[1]]);
 				progress++;
 				break;
 			case 0x03 :
 				tasks[curr].reg[command[1]]=globalMem[command[2]];
-				printf("%x\n",tasks[curr].reg[command[1]]);
+				//	printf("%x\n",tasks[curr].reg[command[1]]);
 				progress++;
 				break;
 			case 0x04 :
 				tasks[curr].reg[command[1]]=globalMem[command[2]+tasks[curr].reg[0]];
-				printf("%x\n",tasks[curr].reg[command[1]]);
+				//	printf("%x\n",tasks[curr].reg[command[1]]);
 				progress++;
 				break;
 			case 0x05 :
 				tasks[curr].localMem[command[2]]=tasks[curr].reg[command[1]];
-				printf("%x\n",tasks[curr].reg[command[1]]);
+				//	printf("%x\n",tasks[curr].reg[command[1]]);
 				progress++;
 				break;
 			case 0x06 :
 				tasks[curr].localMem[command[2]+tasks[curr].reg[0]]=tasks[curr].reg[command[1]];
-				printf("%x\n",tasks[curr].reg[command[1]]);
+				//	printf("%x\n",tasks[curr].reg[command[1]]);
 				progress++;
 				break;
 			case 0x07 :
 				globalMem[command[2]]=tasks[curr].reg[command[1]];
-				printf("%x\n",tasks[curr].reg[command[1]]);
+				//	printf("%x\n",tasks[curr].reg[command[1]]);
 				progress++;
 				break;
 			case 0x08 :
 				globalMem[command[2]+tasks[curr].reg[0]]=tasks[curr].reg[command[1]];
-				printf("%x\n",tasks[curr].reg[command[1]]);
+				//	printf("%x\n",tasks[curr].reg[command[1]]);
 				progress++;
 				break;
-			//-----------registers--------------------
+				//-----------registers--------------------
 			case 0x09:
 				tasks[curr].reg[command[1]]=command[2];
 				progress++;
@@ -360,7 +362,7 @@ int main (int argc, char * argv[]){
 				progress++;
 				break;
 				
-			// --------- branches---------------------
+				// --------- branches---------------------
 				
 			case 0x0F:
 				if (tasks[curr].reg[command[1]]>0){
@@ -397,29 +399,164 @@ int main (int argc, char * argv[]){
 				progress++;
 				break;
 				
-			// ----------- synch ------------------
+				// ----------- synch ------------------
 			case 0x15:
 				globalMem[command[2]]--;
 				if (globalMem[command[2]]<0) {
 					strcpy(tasks[curr].state,"BLOCKED");
 				}
 				tasks[curr].sem=command[2];
+				progress=0;
+				i=0;
+				progress=0;
+				
+				for (i=0;i<notasks;i++){
+					if ((i!=curr)&&(strcmp(tasks[i].state,"BLOCKED"))&&(strcmp(tasks[i].state,"STOPPED"))){
+						if (((!strcmp(tasks[i].state,"SLEEPING"))&&(time(NULL)>tasks[i].waket))){
+							strcpy (tasks[i].state,"READY");
+							tasks[i].waket=0;
+							break;
+						}
+						else if ((strcmp(tasks[i].state,"SLEEPING"))){
+							break;
+						}
+						
+					}
+					
+				}
+				if (i==notasks){
+					printf ("Nowhere to go to from here :(");
+					endflag=1;
+					break;
+				}
+					curr=i;
+					
+					break;
+					
+			case 0x16:
+				globalMem[command[2]]++;
+				
+				if (globalMem[command[2]]<=0) {
+					for (i=0; i<notasks; i++){
+						
+						if (tasks[i].sem==command[2]){
+							strcpy(tasks[i].state,"READY");
+							tasks[i].sem=-1;
+							break;
+						}
+					}
+				}
+				
 				progress++;
 				break;
-			/*case 0x16:
-				globalMem[command[2]]++;
-				if (globalMem[command[2]]<=0) {
+				//----------------varia--------------
+			case 0x17:
 				
-					strcpy(tasks[curr].state,"BLOCKED");
+				progress=0;
+				i=0;
+				for (i=0;i<notasks;i++){
+					if ((i!=curr)&&(strcmp(tasks[i].state,"BLOCKED"))&&(strcmp(tasks[i].state,"STOPPED"))){
+						if (((!strcmp(tasks[i].state,"SLEEPING"))&&(time(NULL)>tasks[i].waket))){
+							strcpy (tasks[i].state,"READY");
+							tasks[i].waket=0;
+							break;
+						}
+						else if ((strcmp(tasks[i].state,"SLEEPING"))){
+							break;
+						}
+						
+					}
+					
 				}
-				tasks[curr].sem=command[2];
-				progress++;
-				break;*/
+				if (i==notasks){
+					printf ("Nowhere to YIELD to :(\n");
+					
+					
+				}
+				else{
+				curr=i;}
+				
+				break;
+				
+			case 0x18:
+				
+				strcpy(tasks[i].state,"SLEEPING");
+				
+				tasks[curr].waket= command[2]+ time(NULL);
+				
+				break;
+			case 0x19:
+				
+				printf ("%d:%d\n", tasks[curr].id, globalMem[command[2]]);
+				
+				
+			case 0x1a:
+				
+				strcpy(tasks[curr].state,"STOPPED");
+			
+				progress=0;
+				i=0;
+				progress=0;
+				i=0;
+				for (i=0;i<notasks;i++){
+					if ((i!=curr)&&(strcmp(tasks[i].state,"BLOCKED"))&&(strcmp(tasks[i].state,"STOPPED"))){
+						if (((!strcmp(tasks[i].state,"SLEEPING"))&&(time(NULL)>tasks[i].waket))){
+							strcpy (tasks[i].state,"READY");
+							tasks[i].waket=0;
+							break;
+						}
+						else if ((strcmp(tasks[i].state,"SLEEPING"))){
+							break;
+						}
+						
+					}
+					
+				}
+				if (i==notasks){
+					printf ("nowhere to go to");
+					endflag=1;
+					break;
+				}
+				curr=i;
+				
+				break;
+				
 		}
-		k++;	
+				if (progress==5){
+					progress=0;
+					i=0;
+					for (i=0;i<notasks;i++){
+						if ((i!=curr)&&(strcmp(tasks[i].state,"BLOCKED"))&&(strcmp(tasks[i].state,"STOPPED"))){
+							
+							if (((!strcmp(tasks[i].state,"SLEEPING"))&&(time(NULL)>tasks[i].waket))){
+								
+								strcpy (tasks[i].state,"READY");
+								tasks[i].waket=0;
+								break;
+							}
+							else if ((strcmp(tasks[i].state,"SLEEPING"))){
+								
+								break;
+							}
+							
+						}
+						
+					}
+					
+					if (i!=notasks){
+						curr=i;
+					}
+					
+				}
+				
+				
+				if (endflag==1){
+					printf ("end of programm\n");
+					break;
+				}
+		}
+		
+		
+		
+		return (0);
 	}
-	
-	
-	
-	return (0);
-}
